8c4081e9722d3630734b485fedfa5be5389088e1
Omer Erdil Albayrak <omer-erdil.albayrak@klarna.com>
2024-04-08 22:21:21 +0200

    Fibonacci numbers with dynamic programming.

--- src/test/java/dev/erdil/FibonacciNumbersDynamicProgramming.java
+++ src/test/java/dev/erdil/FibonacciNumbersDynamicProgramming.java
@@ -2,40 +2,63 @@ package dev.erdil;
 
 import org.junit.jupiter.api.Test;
 
-import static dev.erdil.FibonacciNumbersDynamicProgramming.Solution.fibonacci;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
 public class FibonacciNumbersDynamicProgramming {
 
+    private final Solution s = new Solution();
+
     @Test
     public void shouldFib0is0() {
-        assertThat(fibonacci(0), equalTo(0));
+        assertThat(s.fibonacci(0), equalTo(0));
     }
 
     @Test
     public void shouldFib1is1() {
-        assertThat(fibonacci(1), equalTo(1));
+        assertThat(s.fibonacci(1), equalTo(1));
     }
 
     @Test
     public void shouldFib2is1() {
-        assertThat(fibonacci(2), equalTo(1));
+        assertThat(s.fibonacci(2), equalTo(1));
     }
 
     @Test
     public void shouldFib3is2() {
-        assertThat(fibonacci(3), equalTo(2));
+        assertThat(s.fibonacci(3), equalTo(2));
     }
 
     @Test
     public void shouldFib10is55() {
-        assertThat(fibonacci(10), equalTo(55));
+        assertThat(s.fibonacci(10), equalTo(55));
+    }
+
+    @Test
+    public void shouldFib30is832040() {
+        assertThat(s.fibonacci(30), equalTo(832040));
     }
 
     public class Solution {
-        public static int fibonacci(int n) {
-            return 0;
+        public int fibonacci(int n) {
+            int[] subResults = new int[n + 1];
+            return f(subResults, n);
+        }
+
+        public int f(int[] subResults, int n) {
+            if (n == 0) {
+                return 0;
+            }
+            if (n == 1) {
+                return 1;
+            }
+            if (subResults[n] != 0) {
+                return subResults[n];
+            }
+
+            subResults[n] = f(subResults, n-2) + f(subResults, n-1);
+
+            return subResults[n];
         }
     }
 }
